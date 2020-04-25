e315c1293e4c3f8c30da103c92129a42a628f7e3
walter-grace <nico.zahniser@gmail.com>
2020-04-24 22:30:50 -0700

    new code

---
+++ build/precache-manifest.c6f42688ebe8ca4a1b809cd2183701ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ab5bd7025d09d7df4653f55a84bdf0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "413fd9cf5c210e9339b3",
+    "url": "/static/css/main.fe715396.chunk.css"
+  },
+  {
+    "revision": "d0a26ad2ba73d93c2caa",
+    "url": "/static/js/2.c116b160.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c116b160.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "413fd9cf5c210e9339b3",
+    "url": "/static/js/main.8ec1253a.chunk.js"
+  },
+  {
+    "revision": "48fd0eb7ad446bdda244",
+    "url": "/static/js/runtime-main.c4e8154c.js"
+  }
+]);
